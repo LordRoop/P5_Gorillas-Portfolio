3782cca17dcde1cd9c47f2d4b87ce23ba584fd42
PedroBMedeiros <pedrinho.medeiros.2003@gmail.com>
2020-10-30 18:20:15 -0700

    Navbar 4.25

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ def home():
 def projects():
     #Flask import uses Jinga to render HTML
     return render_template("projects.html")
+
+@app.route('/journals')
+def journals():
+    #Flask import uses Jinga to render HTML
+    return render_template("journals.html")
+
 if __name__ == "__main__":
     #runs the application on the repl development server
     app.run(debug=True, port=' 5000', host='192.168.86.51')
