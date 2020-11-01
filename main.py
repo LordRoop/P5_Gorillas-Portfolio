f70499fef4a1526d4053d9978a9a2aec0f4d5a3c
PedroBMedeiros <pedrinho.medeiros.2003@gmail.com>
2020-10-31 21:17:34 -0700

    4.80

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 from flask import Flask, render_template
-#create a Flask instance
+
 app = Flask(__name__)
 
-#connects default URL to a function
+
 @app.route('/')
 def home():
-    #Flask import uses Jinga to render HTML
+
     return render_template("home.html")
 
 @app.route('/projects')
 def projects():
-    #Flask import uses Jinga to render HTML
+
     return render_template("projects.html")
 
 @app.route('/journals')
 def journals():
-    #Flask import uses Jinga to render HTML
+
     return render_template("journals.html")
 
 @app.route('/pedro')
 def pedro():
-    #Flask import uses Jinga to render HTML
+
     return render_template("pedro.html")
 
 if __name__ == "__main__":
-    #runs the application on the repl development server
     app.run(debug=True, port=' 5000', host='192.168.86.51')
