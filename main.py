aa51ab1b7f421717b420f5143c8394307fb94602
Colin Tran <colint012204@gmail.com>
2020-11-06 23:00:50 -0800

    changed ip 70.95.189.45:5000 (html ip) 192.168.86.51 (main.py)

--- main.py
+++ main.py
@@ -44,4 +44,4 @@ def colin():
     return render_template("colin.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=' 5000', host='192.168.86.51')
+    app.run(debug=True, port=' 5000', host='127.0.0.10')
